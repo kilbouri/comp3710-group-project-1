9eefdee7a1b0cfc5e39c06d1e6bc5bc2c1c2f210
SquareWheelBike <colefuerth@gmail.com>
2022-02-20 16:26:43 -0500

    created batch class for running batches of games

--- src/game.py
+++ src/game.py
@@ -11,6 +11,9 @@ choiceMap = {
     0: "cooperate"
 }
 
+def __mean(_: list) -> float:
+    return sum(_) / len(_)
+
 class Game:
     # Contains relevant functions to run a game between two agents
     def __init__(self, agentA: Agent, agentB: Agent, gameLength: int, debug: bool = False):
@@ -38,3 +41,28 @@ class Game:
             aChoice = agentA.choose()
             bChoice = agentB.choose()
             yield aChoice, bChoice
+
+class Batch:
+    # Class for batch testing will:
+
+    # Initialize with two agent class types as parameters, as well as the game length in turns and the total number of games
+    # Run N games between the two, recording the results of each game
+    # Return a tuple, containing the average (fitness) score of each agent across the batch
+
+    def __init__(self, classA, classB, gameLength:int=50, numGames:int=50, memorySize:int=3):
+        self.classA = classA
+        self.classB = classB
+        self.gameLength = gameLength
+        self.numGames = numGames
+        self.fitnessA = []
+        self.fitnessB = []
+
+    def run(self):
+        for _ in range(self.numGames):
+            agentA = self.classA()
+            agentB = self.classB()
+            game = Game(agentA, agentB, self.gameLength)
+            fitnessA, fitnessB = game.play()
+            self.fitnessA.append(fitnessA)
+            self.fitnessB.append(fitnessB)
+        return __mean(self.fitnessA), __mean(self.fitnessB)
