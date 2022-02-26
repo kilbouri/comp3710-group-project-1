54e52579b6753e3ea8945e1aa6d7a7ee9a6c9ab9
SquareWheelBike <colefuerth@gmail.com>
2022-02-26 16:31:39 -0500

    added some training batches

---
+++ src/train.py
@@ -0,0 +1,69 @@
+from game import Game, Batch, mean, REWARD_TABLE
+from Agents import *
+from compare import Comparison, Population
+from pandas import DataFrame
+import csv
+from os import path
+
+# Collection of different training functions]
+
+agentStrings = {
+    'RandomAgent': RandomAgent,
+    'CooperativeAgent': CooperativeAgent,
+    'DefectiveAgent': DefectiveAgent,
+    'TFTAgent': TFTAgent,
+    'DefectiveAgent': DefectiveAgent,
+    'TFNTAgent': TFNTAgent,
+    'STFTAgent': STFTAgent,
+    'PavlovAgent': PavlovAgent,
+    'GeneticAgent': GeneticAgent
+}
+
+def bulkTrain():
+    # Run a set of training against each simple agent. Record the results in a csv file.
+    # 1000 generations, 10 games per generation, 500 agents, 64 turns per game, 3 memory
+    memsize = 6
+    popsize = 500
+    games = 10
+    turns = 64
+    generations = 1000
+    csvpath = '../dualParentBulkTrained6mem.csv'
+
+    allAgents = list(agentStrings.keys())
+    if not path.exists(csvpath):
+        with open(csvpath, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Opponent', 'Fittest', 'Average', 'memSize', 'nGens', 'gameLen', 'nGames'])
+    with open(csvpath, 'a') as csvfile:
+        writer = csv.writer(csvfile)
+        for agent in allAgents:
+            pop = Population(memorySize=memsize, populationSize=popsize)
+            pop.train(generations, opponentType=agentStrings[agent], gameLength=turns, numGames=games)
+            fittest = pop.fittestChromosome()
+            average = pop.averageChromosome()
+            writer.writerow([agent, fittest, average, memsize, generations, turns, games])
+
+def bulkTrainSingle():
+    # Run a set of training against each simple agent. Record the results in a csv file.
+    # 1000 generations, 10 games per generation, 500 agents, 64 turns per game, 3 memory
+    # This one uses single reproduction
+    memsize = 6
+    popsize = 500
+    games = 10
+    turns = 64
+    generations = 1000
+    csvpath = '../dualParentBulkTrained6mem.csv'
+
+    allAgents = list(agentStrings.keys())
+    if not path.exists(csvpath):
+        with open(csvpath, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Opponent', 'Fittest', 'Average', 'memSize', 'nGens', 'gameLen', 'nGames'])
+    with open(csvpath, 'a') as csvfile:
+        writer = csv.writer(csvfile)
+        for agent in allAgents:
+            pop = Population(memorySize=memsize, populationSize=popsize)
+            pop.train(generations, opponentType=agentStrings[agent], gameLength=turns, numGames=games, singleReproduction=True)
+            fittest = pop.fittestChromosome()
+            average = pop.averageChromosome()
+            writer.writerow([agent, fittest, average, memsize, generations, turns, games])
